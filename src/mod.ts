747c3e3f70e3d12471cd297c43f80400376ba67b
marmooo <marmooo@users.noreply.github.com>
2026-06-05 11:17:22 +0900

    improve export

--- src/mod.ts
+++ src/mod.ts
@@ -1,3 +1,6 @@
+export * from "./Generator.ts";
+export * from "./Structs.ts";
+export * from "./AudioData.ts";
+export * from "./Voice.ts";
 export * from "./Parser.ts";
 export * from "./SoundFont.ts";
-export * from "./Generator.ts";
